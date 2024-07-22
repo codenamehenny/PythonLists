2d0e72550b6974687e6521e1a2d75d8b2d7041fc
codenamehenny <genesis09m@hotmail.com>
2024-07-22 12:30:07 -0400

    print list and average

---
+++ PythonListTrans.py
@@ -0,0 +1,9 @@
+# Task 1: Given the list of grades, Sort the grades in descending order and print the sorted list.
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort() # sorts list in order
+print(grades)
+
+#Task 2: Calculate the average grade and print it.
+avg = sum(grades) / len(grades) # calculates the sum of the list and divde that by the number of integers in the list.
+print(avg)
